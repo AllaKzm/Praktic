af57a22dab2733f8e031423bfcd99c8a96b4fe29
Alla <roi_mukh@mail.ru>
2022-06-12 20:23:17 +0300

    check

--- bd.py
+++ bd.py
@@ -26,6 +26,17 @@ class Database:
         cursor.close()
         return requests
 
+    def insertRequests(self, number_request, date_create, time_request, number_client, services,
+                       status_request, rental_time):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            f"INSERT INTO requests"
+            f"(`number_request`, `date_create`, `time_request`, `number_client`, `services`, `status_request`, `rental_time`)"
+            f"VALUES ('{number_request}', '{date_create}', '{time_request}', '{number_client}', '{services}', '{status_request}', '{rental_time}')"
+        )
+        cursor.close()
+        self.connection.commit()
+
     def getEmployeers(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM EMPLOYEERS")
@@ -46,6 +57,17 @@ class Database:
         history = cursor.fetchall()
         cursor.close()
         return history
+
+    def check_login(self):
+        log = []
+        cursor = self.connection.cursor()
+        cursor.execute(f"""SELECT login FROM employeers""")
+        rows = cursor.fetchall()
+        for i in rows:
+            for j in i:
+                log.append(j)
+        return log
+        cursor.close()
 
     def get_log(self, login):
         log = []
